e27020575c9293f053ed9c48ea04c894c0754346
martatomalska <m@example.com>
2019-03-03 22:45:21 +0100

    printing numbers

--- src/main/FizzBuzz.java
+++ src/main/FizzBuzz.java
@@ -3,14 +3,21 @@ package main;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		for (int i=1; i<=100; i++) {
+			System.out.println(fizzBuzzOrNumber(i));
+		}
 
 	}
 	
 	
-	public String fizzBuzzOrNumber (int number) {
+	public static String fizzBuzzOrNumber (int number) {
 		
-		return null;
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(number);
+		
+		return sb.toString();
 	}
 
 }
